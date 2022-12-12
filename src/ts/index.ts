e56d5c4e80d07082d4ab3b98124ac5452e12dd85
PSILVA999 <p.silva0399@gmail.com>
2022-12-12 09:45:11 -0300

    transacoes

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,28 +1,13 @@
 import fetchData from "./fetchData"
-
-type TransacaoPagamento = 'Boleto' | 'Cartão de Crédito'
+import normalizeTransacao from "./normalizeTransacao"
 
 async function handleData() {
-  interface TransacaoAPI {
-    Nome: string;
-    ID: number;
-    Data: string;
-
-    Status: TransacaoPagamento;
-    Email: string;
-
-    ['Valor (R$)']: string;
-    ['Forma de Pagamento']: TransacaoPagamento;
-    ['Cliente Novo']: number;
-  }
-
   const data = await fetchData<TransacaoAPI[]>('https://api.origamid.dev/json/transacoes.json')
 
-  if (data) {
-    data.forEach(item => {
-      console.log(item["Valor (R$)"])
-    })
-  }
+  if (!data) return
+  const transacoes = data.map(normalizeTransacao)
+
+  console.log(transacoes)
 }
 
 export function handleBody() {

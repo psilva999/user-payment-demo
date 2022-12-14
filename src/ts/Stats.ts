5c788139fe6ff070a1d5b8950851ce400f29653b
PSILVA999 <p.silva0399@gmail.com>
2022-12-14 08:28:38 -0300

    status

--- src/ts/Stats.ts
+++ src/ts/Stats.ts
@@ -1,3 +1,5 @@
+import countBy from "./countBy";
+
 type TransacaoValor = Transacao & { valor: number }
 
 function filterValue(transacao: Transacao): transacao is TransacaoValor {
@@ -6,16 +8,30 @@ function filterValue(transacao: Transacao): transacao is TransacaoValor {
 
 export default class Stats {
   private transacoes
-  value
+
+  value;
+  pagamento;
+  status;
 
   constructor(transacoes: Transacao[]) {
     this.transacoes = transacoes
     this.value = this.setValue()
+
+    this.pagamento = this.setPagamento()
+    this.status = this.setStatus()
   }
 
   private setValue() {
     return this.transacoes.filter(filterValue).reduce((acc, item) => {
       return acc + item.valor
     }, 0)
+  }
+
+  private setPagamento() {
+    return countBy(this.transacoes.map(({ pagamento }) => pagamento))
+  }
+
+  private setStatus() {
+    return countBy(this.transacoes.map(({ status }) => status))
   }
 }
